84f8c63a8a7c0eae9490d646c24c653daf98a7ff
root <root@localhost.localdomain>
2018-01-24 00:41:12 +0800

    first commit

---
+++ app/src/main/java/com/example/root/myfirstapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.root.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "you click button", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        Log.d(TAG, "onCreate: execute");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "you clicked add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "you clicked remove", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
