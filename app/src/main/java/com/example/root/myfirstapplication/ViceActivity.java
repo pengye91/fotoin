2cb523c418736d5fd273fcb05fe18510c84f35e3
xyp <xyp@kali.beijing>
2018-01-24 19:52:44 +0800

    added new activity

---
+++ app/src/main/java/com/example/root/myfirstapplication/ViceActivity.java
@@ -0,0 +1,13 @@
+package com.example.root.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vice);
+    }
+}
